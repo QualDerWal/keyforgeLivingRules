c793fdb2e2d9970e0d4c4669fd3a143d09d40743
Alexander Sauer <alexander.sauer@outlook.com>
2018-11-25 16:53:21 +0100

    add js, skeleton js, and onload function

---
+++ currentRules.js
@@ -0,0 +1,5 @@
+function fillDom() {
+    var container = document.getElementsByClassName( 'container' );
+    // append to the element's content
+    container[0].innerHTML += '<div class="row"> <div class="three columns">three</div><div class="nine columns">nine</div></div>';
+}

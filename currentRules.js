92c052cdedd250671fe89b871ed07b90f1bf6d52
Alexander Sauer <alexandersauer@alexanders-mbp.ecx.local>
2018-11-26 14:31:16 +0100

    add lists

--- currentRules.js
+++ currentRules.js
@@ -14,14 +14,35 @@ xmlhttp.send(null);
 function fillDom() {
     var container = document.getElementsByClassName( 'container' );
     // append to the element's content
-    var titleList = json.sections;
-    console.log(titleList)
-    // container[0].innerHTML += '<div class="row"> <' + titleList[0].type + ' class="three columns">'+ titleList[0].title + '</'+ titleList[0].type +'><div class="nine columns">nine</div></div>';
+    var ruleSections = json.sections;
+    console.log(ruleSections)
+    // container[0].innerHTML += '<div class="row"> <' + ruleSections[0].type + ' class="three columns">'+ ruleSections[0].title + '</'+ ruleSections[0].type +'><div class="nine columns">nine</div></div>';
 
-    for (var index = 0; index < titleList.length; index++) {
-        container[0].innerHTML += '<' + titleList[index].type +'>'+ titleList[index].title + '</'+ titleList[index].type +'>'+ '<div>'+ titleList[index].paragraph +'</div>';
-
-        
+    for (var index = 0; index < ruleSections.length; index++) {
+        if(ruleSections[index].type){
+            container[0].innerHTML += '<' + ruleSections[index].type +'>'+ ruleSections[index].title + '</'+ ruleSections[index].type +'>'+ '<p>'+ ruleSections[index].paragraph +'</p>';
+        }
+        else {
+            container[0].innerHTML += '<p>'+ ruleSections[index].paragraph +'</p>';
+        }
+        if(ruleSections[index].oList){
+            var temp = "";
+            temp += "<ol>"
+            for (let item = 0; item < ruleSections[index].oList.length; item++) {
+                temp += '<li>'+ ruleSections[index].oList[item] +'</li>';
+            }
+            temp += "</ol>"
+            container[0].innerHTML += temp;
+        }
+        if(ruleSections[index].uList){
+            var temp = "";
+            temp += "<ul>"
+            for (let item = 0; item < ruleSections[index].uList.length; item++) {
+                temp += '<li>'+ ruleSections[index].uList[item] +'</li>';
+            }
+            temp += "</ul>"
+            container[0].innerHTML += temp;
+        }
     }
 }
 

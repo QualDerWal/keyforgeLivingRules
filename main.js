a3ec67f4507152733e312e6713927a638acc9ab2
Maximilian Henrich <maximilian.henrich@ecx.io>
2018-11-26 22:30:36 +0100

    Add markdown lib

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-
-
 var app = new Vue({
-    el: '#app',
-    data: {
-        jsonData: []
-    },
-    mounted: function () {
-        var self = this;
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open('GET', './currentRules.json', true);
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState == 4) {
-                if (xmlhttp.status == 200) {
-                    console.log(xmlhttp)
-                    self.jsonData = JSON.parse(xmlhttp.responseText).sections;
-                    console.log(self.jsonData);
-                }
-            }
-        };
-        xmlhttp.send(null);
-        
-    },
-})
+  el: "#app",
+  data: {
+    jsonData: []
+  },
+  filters: {
+    markdown: function(value) {
+      if (!value || typeof value !== 'string') return "";
+      return marked(value);
+    }
+  },
+  mounted: function() {
+    var self = this;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "./currentRules.json", true);
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 200) {
+          console.log(xmlhttp);
+          self.jsonData = JSON.parse(xmlhttp.responseText).sections;
+          console.log(self.jsonData);
+        }
+      }
+    };
+    xmlhttp.send(null);
+  }
+});
